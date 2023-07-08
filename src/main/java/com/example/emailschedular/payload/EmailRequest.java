d7608842100e38fa9be1291b2eb5d2f72dcd5b50
nibchee <nirbhaybansal28@gmail.com>
2023-07-09 01:48:34 +0530

    Email Schedular Request & Response Objects

---
+++ src/main/java/com/example/emailschedular/payload/EmailRequest.java
@@ -0,0 +1,30 @@
+package com.example.emailschedular.payload;
+
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Data
+public class EmailRequest {
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String subject;
+
+    @NotEmpty
+    private String body;
+
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @NotNull
+    private ZoneId timeZone;
+}
