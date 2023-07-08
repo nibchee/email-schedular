2e400efbed9589286d71a768c38e69de480588a1
nibchee <nirbhaybansal28@gmail.com>
2023-07-09 02:10:18 +0530

    Job & Trigger functions created

---
+++ src/main/java/com/example/emailschedular/quartz/job/EmailJob.java
@@ -0,0 +1,11 @@
+package com.example.emailschedular.quartz.job;
+
+import org.quartz.JobExecutionContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class EmailJob extends QuartzJobBean {
+
+    protected void executeInternal(JobExecutionContext jobExecutionContext){
+
+    }
+}
---
+++ src/main/java/com/example/emailschedular/web/EmailSchedularController.java
@@ -0,0 +1,37 @@
+package com.example.emailschedular.web;
+
+import com.example.emailschedular.payload.EmailRequest;
+import com.example.emailschedular.quartz.job.EmailJob;
+import org.quartz.*;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+public class EmailSchedularController {
+
+    private JobDetail buildJobDetail(EmailRequest scheduleEmailRequest) {
+        JobDataMap jobDataMap = new JobDataMap();
+
+        jobDataMap.put("email", scheduleEmailRequest.getEmail());
+        jobDataMap.put("subject", scheduleEmailRequest.getSubject());
+        jobDataMap.put("body", scheduleEmailRequest.getBody());
+
+        return JobBuilder
+                .newJob(EmailJob.class)
+                .withIdentity(UUID.randomUUID().toString(), "email-jobs")
+                .withDescription("Send Email Job")
+                .usingJobData(jobDataMap)
+                .storeDurably().build();
+    }
+
+    private Trigger buildTrigger(JobDetail jobDetail, ZonedDateTime startAt) {
+        return TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .withIdentity(jobDetail.getKey().getName(), "email-triggers")
+                .withDescription("Send Email Trigger")
+                .startAt(Date.from(startAt.toInstant()))
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withMisfireHandlingInstructionFireNow())
+                .build();
+    }
+}
