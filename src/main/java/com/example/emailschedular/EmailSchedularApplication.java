6739b196be79db2568493d74f8d3d69f90522ed8
nibchee <nirbhaybansal28@gmail.com>
2023-07-08 23:39:52 +0530

    SpringBoot App Initialised with specific dependencies

---
+++ src/main/java/com/example/emailschedular/EmailSchedularApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailschedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSchedularApplication.class, args);
+	}
+
+}
